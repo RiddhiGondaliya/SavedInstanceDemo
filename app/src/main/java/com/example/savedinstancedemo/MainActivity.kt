c6a90eb0929fe18baa6b1d8961fc7c7efac421b0
Paresh Dudhat <dudhatparesh@gmail.com>
2020-11-25 11:41:30 +0530

    saveInstanceState

--- app/src/main/java/com/example/savedinstancedemo/MainActivity.kt
+++ app/src/main/java/com/example/savedinstancedemo/MainActivity.kt
@@ -2,25 +2,36 @@ package com.example.savedinstancedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
+import java.util.stream.DoubleStream.concat
 
 class MainActivity : AppCompatActivity() {
 
+
+    val KEY_VALUE = "value"
     lateinit var string: String
     lateinit var editText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val editText = findViewById<EditText>(R.id.edt_type)
-        string = editText.toString()
+        val text = editText.text.toString()
 
-    }
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        var dot :String = "."
-        var mydata :String = string + dot
-        print(mydata)
+        if (savedInstanceState != null) {
+            string = savedInstanceState.getString(KEY_VALUE, text)
+            Log.i("message","data not modified")
+        }
+
+        fun onSaveInstanceState(outState: Bundle) {
+            super.onSaveInstanceState(outState)
+           // var dot :String = "."
+           //var mydata : String = data + dot
+            outState.putString(KEY_VALUE, string)
+            Log.i("message","saved data")
 
 
+        }
     }
 }
